c6c7becdc9975a77b0b4f42da0ba6690efd4c8fb
Pilipalapiu <1170878836@qq.com>
2021-10-01 18:04:31 +0800

    debug

--- Heap/PQ_ComplHeap.h
+++ Heap/PQ_ComplHeap.h
@@ -13,26 +13,95 @@ int get_PQmax(PQ* PQ){
     return PQ->data[0];
 }
 
-int getParentIndex(int index){
-    return (index-1)>>1;
+int getParentIndex(PQ* H,int index){
+    if( (index-1)>>1 > 0||(index-1)>>1 == 0 )
+        return (index-1)>>1;
+    else 
+        return -1;
 }
 
-int getLcIndex(int index){
-    return index<<1 + 1;
+int getLcIndex(PQ* H,int index){
+    if( (index*2 + 1) < H->LastIndex||(index*2 + 1) == H->LastIndex )
+        return index*2 + 1;
+    else 
+        return -1;
 }
 
-int getRcIndex(int index){
-    return index<<1 + 2;
+int getRcIndex(PQ* H,int index){
+    if((index*2 + 2) < H->LastIndex||(index*2 + 2) == H->LastIndex)
+        return index*2 + 2;
+    else
+        return -1;
+}
+//判断一个索引的合法性
+bool InHeap(PQ* H, int index){
+   if((index > -1)&&(index < H->LastIndex + 1)) 
+        return true;
+    else 
+        return false;
+}
+//右孩子值
+int RChild(PQ* H, int index){
+    if(InHeap(H,getRcIndex(H,index)))
+        return H->data[getRcIndex(H,index)];
+    else
+        return -999;
+}
+//左孩子值
+int LChild(PQ* H, int index){
+    if(InHeap(H,getLcIndex(H,index)))
+        return H->data[getLcIndex(H,index)]; 
+    else
+        return -999;
+}
+
+//是否有左孩子
+bool HasLc(PQ* H, int index){
+    if(InHeap(H,getLcIndex(H,index))) 
+        return true; 
+    else 
+        return false;
+}
+//是否有右孩子
+bool HasRc(PQ* H, int index){
+    if(InHeap(H,getRcIndex(H,index))) 
+        return true; 
+    else
+        return false;
+}
+//两者取最大者索引，相等前者优先
+int Bigger(PQ* H, int index1,int index2){
+    if((H->data[index1] > H->data[index2])||(H->data[index1] == H->data[index2]))
+        return index1;
+    else
+        return index2;
+}
+//选取当前节点的最大孩子
+int getMaxChild(PQ* H, int index){
+    if(HasLc(H,index))
+    {
+        if(HasRc(H,index))
+            return Bigger(H,getLcIndex(H,index),getRcIndex(H,index));
+        else
+            return getLcIndex(H,index);
+    }
+    else
+    {
+        if(HasRc(H,index))
+            return getRcIndex(H,index);
+        else
+            return -1;
+    }
 }
 
 //上滤
 int up(PQ* PQ, int index){
     while (index!=0)
     {
-        if(PQ->data[index] > PQ->data[getParentIndex(index)])
+        if(PQ->data[index] > PQ->data[getParentIndex(PQ,index)])
         {
-            swap(&(PQ->data[index]), &(PQ->data[getParentIndex(index)]));
-            index = getParentIndex(index);
+            swap(&(PQ->data[index]), &(PQ->data[getParentIndex(PQ,index)]));
+            index=getParentIndex(PQ,index);
         }
         else
             break;
@@ -41,24 +110,19 @@ int up(PQ* PQ, int index){
 }
 //下滤
 int down(PQ* PQ, int index){
-    while (index < PQ->LastIndex )
-    {
-        if(PQ->data[index] < PQ->data[getLcIndex(index)])
+    while(1){
+        if(getMaxChild(PQ,index)!=-1)
         {
-            swap(&(PQ->data[index]), &(PQ->data[getLcIndex(index)]));
-            index = getLcIndex(index);
-            break;
-        }
-        if (PQ->data[index] < PQ->data[getRcIndex(index)])
-        {
-            swap(&(PQ->data[index]), &(PQ->data[getRcIndex(index)]));
-            index = getRcIndex(index);
-            break;
+            if(PQ->data[index] < PQ->data[getMaxChild(PQ,index)])
+            {
+                int tmp = getMaxChild(PQ,index);
+                swap(&(PQ->data[index]), &(PQ->data[tmp]));
+                index = tmp;
+            }
         }
         else
             break;
     }
-    return index;
 }
 
 int del_PQmax(PQ* PQ){
@@ -90,7 +154,7 @@ void BF_Heapify(PQ** H){
 //Floyd建堆
 void Floyd_Heapify(PQ** H){
 
-    int buf;
+    int buf=0;
 
     printf("\nEnter the value:\n");
     scanf("%d",&buf);
@@ -100,7 +164,8 @@ void Floyd_Heapify(PQ** H){
         printf("\nEnter the value:\n");
         scanf("%d",&buf);
     }
-    for (int i = (*H)->LastIndex; i < 0; i--)
+
+    for (int i = (*H)->LastIndex; i > -1; i--)
     {
         down(*H,i);
     }
--- test/main.c
+++ test/main.c
@@ -5,6 +5,4 @@ int main(){
     H->LastIndex = -1;
     Floyd_Heapify(&H);
     show(H->data,(H->LastIndex)+1);
-    Floyd_Heapify(&H);
-    show(H->data,(H->LastIndex)+1);
 }
